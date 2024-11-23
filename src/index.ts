22fcc1e12b25d9638b08d6bfd5e7e17056a9a3a9
YannisFouzi <yfouzi.dev@gmail.com>
2024-11-23 09:13:15 +0100

    suppresion du backend gallery

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import express from "express";
 import { connectDB } from "./config/database";
 import authRoutes from "./routes/auth";
 import eventRoutes from "./routes/events";
-import galleryRoutes from "./routes/gallery";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,19 +18,6 @@ app.use(express.json());
 // Routes
 app.use("/api/events", eventRoutes);
 app.use("/api/auth", authRoutes);
-app.use(
-  "/api/gallery",
-  (req, res, next) => {
-    console.log("Requête reçue sur /api/gallery:", {
-      method: req.method,
-      path: req.path,
-      body: req.body,
-      headers: req.headers,
-    });
-    next();
-  },
-  galleryRoutes
-);
 
 // Connect to database and start server
 connectDB().then(() => {
